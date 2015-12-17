1fc5ec71b44ba85c4c8f56405b2ce4872dff9da4
gunnhildurf <gunnhildurf@gmail.com>
2015-12-17 20:49:12 +0000

    acceptance script fix

--- acceptance_script.sh
+++ acceptance_script.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-export ACCEPTANCE_URL=http://192.168.33.15:9000 && grunt mochaTest:acceptance
+export ACCEPTANCE_URL=http://192.168.33.15:9000 && export PATH="/usr/local/bin:$PATH" && grunt mochaTest:acceptance
